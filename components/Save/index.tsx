df1f2ec0ff074d5ac7cdc0e7613d182b71cea773
jason <jason@hedmans.org>
2023-01-08 22:11:17 -0500

    update save page

--- components/Save/index.tsx
+++ components/Save/index.tsx
@@ -1,23 +1,34 @@
 import React from 'react'
 
-import { Box } from '@chakra-ui/react'
+import { Skeleton, VStack } from '@chakra-ui/react'
 
 import Activity from './Activity'
 import Earned from './Earned'
 import EWalletBanner from './EWalletBanner'
 
 import { bannerHeight } from './EWalletBanner'
+import useStudentData from '../../hooks/useStudentData'
 
 const Save : React.FC = () => {
+
+  const studentData = useStudentData();
+
+  if(!studentData) {
+    return <Skeleton />
+  }
+
   return (
-    <Box 
+    <VStack 
       w='100%'
       pt={bannerHeight}
+      spacing={8}
     >
         <EWalletBanner />
-        <Activity />
+        <Activity 
+          userId={studentData.userId}
+        />
         <Earned />
-    </Box>
+    </VStack>
   )
 }
 
